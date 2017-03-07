19c094b6c553daf49eee0088b3adf003e30bc1a4
ninoko1995 <ninomiya.infratop@gmial.com>
2017-03-07 14:23:43 +0000

    finish level1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -22,7 +22,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-      redirect_to @book, notice: 'Book was successfully created.' }
+      redirect_to @book, notice: 'Book was successfully created.' 
     else
       render :index
     end
@@ -32,7 +32,7 @@ class BooksController < ApplicationController
   # PATCH/PUT /books/1.json
   def update
     if @book.update(book_params)
-      redirect_to @book, notice: 'Book was successfully updated.' }
+      redirect_to @book, notice: 'Book was successfully updated.' 
     else
       render :edit
     end
@@ -42,7 +42,7 @@ class BooksController < ApplicationController
   # DELETE /books/1.json
   def destroy
     @book.destroy
-    redirect_to books_url, notice: 'Book was successfully destroyed.' }
+    redirect_to books_url, notice: 'Book was successfully destroyed.'
   end
 
   private
