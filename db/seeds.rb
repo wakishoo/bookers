86f53a6f0e7ad0f0ee48d9ad4835f80d17ff51d4
ninoko1995 <ninomiya.infratop@gmial.com>
2017-03-18 10:57:50 +0000

    modefy gemfile & seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+	name:'nino',
+	email: 'ninoko1995@yahoo.co.jp',
+	password: 'ninoninomiyamiya1995',
+	introduction: '管理者アカウントです'
+	)
+
+
+20.times do |i|
+	User.create!(
+		name:"#{i}#{i}",
+		email: "#{i}@#{i}",
+		password: "#{i}#{i}#{i}#{i}#{i}#{i}",
+		introduction: "週に#{i}冊は本読んでます！よろしくお願いします！"
+	 )
+end
